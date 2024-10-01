1655c17814288150e83ce488a80cecaa9054a606
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-01 15:52:32 +0300

    404 Not Found

--- client/src/Components/Errors/NotFound.jsx
+++ client/src/Components/Errors/NotFound.jsx
@@ -5,9 +5,9 @@ const NotFound = () => {
     return (
         <>
             <section className="bg-white ">
-                <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+                <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12 text-red-600">
                     <div className="wf-ull lg:w-1/2">
-                        <p className="text-sm font-medium text-main_color_darker ">404 error</p>
+                        <p className="text-sm font-medium text-main_color_darker ">404 Error</p>
                         <h1 className="mt-3 text-2xl font-semibold text-gray-800 ">Page not found</h1>
                         <p className="mt-4 text-gray-500 ">Sorry, the page you are looking for does not exist.Here are some helpful links:</p>
 
