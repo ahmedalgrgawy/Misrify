fb91b25b3713648cc86f16a0754dea5c04ddc764
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-10-14 01:24:10 +0300

    Connect Database

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-import { helloFun } from './controllers/hello.js';
+import { connectDb } from './lib/connectDb.js';
 
 dotenv.config()
 
@@ -17,13 +17,8 @@ app.use(cookieParser())
 
 // app.use('/api/')  // for next routes
 
-app.use('/', helloFun)
-
-console.log(process.env.NODE_ENV);
-
 app.listen(port, () => {
-    console.log('Server Running with: ' + process.env.NODE_ENV + 'Environment');
-    // dbConnection
-    console.log('Server Running on port: ' + port)
+    console.log('Server Running in ' + process.env.NODE_ENV + 'Environment on port ' + port);
+    connectDb()
 })
 
