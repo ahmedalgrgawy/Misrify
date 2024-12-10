4527d14f35326f411e90f2a38c6aabe4ffbbe49a
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-10 17:28:09 +0200

    connect redis, cloudinary

--- server/lib/connectDb.js
+++ server/lib/connectDb.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose"
 
 export const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log('MongoDb Connected');
-
+        const res = await mongoose.connect(process.env.MONGO_URL)
+        console.log('MongoDb Connected ' + res.connection.name);
     } catch (error) {
         console.log("Error :" + error.message);
         process.exit(1)
