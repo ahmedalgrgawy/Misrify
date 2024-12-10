e763f356963f710ec71d9185e6b02119506ef236
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-10 18:47:29 +0200

    order model

---
+++ server/models/order.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User is required"],
+    },
+    Coupon: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Coupons",
+    },
+    orderItems: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: [true, "Product is required"],
+            },
+            quantity: {
+                type: Number,
+                required: [true, "Quantity is required"],
+            },
+            color: {
+                type: String,
+                required: [true, "Color is required"],
+            },
+            size: {
+                type: String,
+                required: [true, "Size is required"],
+            },
+            price: {
+                type: Number,
+                required: [true, "Price is required"],
+            },
+            total: {
+                type: Number,
+                required: [true, "Total is required"],
+            }
+        }
+    ],
+    shippingAddress: {
+        type: String,
+        required: [true, "Shipping address is required"],
+    },
+    shippingMethod: {
+        type: String,
+    },
+    trackCode: {
+        type: String,
+        required: [true, "Tracking code is required"],
+    },
+    status: {
+        type: String,
+        enum: ["Pending", ""],
+    },
+    totalPrice: {
+        type: Number,
+    }
+
+}, { timestamps: true })
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order
