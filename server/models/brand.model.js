1c6d12c76d72cd81a64dd7eafac82ddc8f51e8a3
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-10 18:11:59 +0200

    product, category and brand model

---
+++ server/models/brand.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const brandSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        unique: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "Owner is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    }
+}, { timestamps: true })
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+export default Brand
---
+++ server/models/product.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: [true, "Category is required"],
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Brand",
+        required: [true, "Category is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    quantityInStock: {
+        type: Number,
+        required: [true, "Quantity is required"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"],
+    },
+    colors: {
+        type: [String],
+    },
+    sizes: {
+        type: [String],
+    },
+    imgUrl: {
+        type: String,
+        required: [true, "Image is required"],
+    },
+    isDiscounted: {
+        type: Boolean,
+        default: false,
+    },
+    discountAmount: {
+        type: Number,
+        default: 0,
+    },
+    isApproved: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
