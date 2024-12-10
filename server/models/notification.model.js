9d6e78f1f642cd095091c39693d4aac1ea553480
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-10 19:26:21 +0200

    review - notification models

---
+++ server/models/notification.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    receiver: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "Receiver is required"],
+        }
+    ],
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Brand",
+        required: [true, "Sender is required"],
+    },
+    content: {
+        type: String,
+        required: [true, "Message is required"],
+    },
+    type: {
+        type: String,
+        enum: ["order", "product", "coupon"],
+        required: [true, "Type is required"],
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification
