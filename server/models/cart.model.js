e1a82f828782214592174fc3b2f8cbc22d8e3e86
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-10 19:09:19 +0200

    seperate orderItem,cartItem models

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -8,31 +8,8 @@ const cartSchema = new mongoose.Schema({
     },
     cartItems: [
         {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: [true, "Product is required"],
-            },
-            quantity: {
-                type: Number,
-                required: [true, "Quantity is required"],
-            },
-            color: {
-                type: String,
-                required: [true, "Color is required"],
-            },
-            size: {
-                type: String,
-                required: [true, "Size is required"],
-            },
-            price: {
-                type: Number,
-                required: [true, "Price is required"],
-            },
-            total: {
-                type: Number,
-                required: [true, "Total is required"],
-            }
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "CartItem",
         }
     ],
     totalPrice: {
--- server/models/order.model.js
+++ server/models/order.model.js
@@ -12,31 +12,8 @@ const orderSchema = new mongoose.Schema({
     },
     orderItems: [
         {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product",
-                required: [true, "Product is required"],
-            },
-            quantity: {
-                type: Number,
-                required: [true, "Quantity is required"],
-            },
-            color: {
-                type: String,
-                required: [true, "Color is required"],
-            },
-            size: {
-                type: String,
-                required: [true, "Size is required"],
-            },
-            price: {
-                type: Number,
-                required: [true, "Price is required"],
-            },
-            total: {
-                type: Number,
-                required: [true, "Total is required"],
-            }
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "OrderItem",
         }
     ],
     shippingAddress: {
