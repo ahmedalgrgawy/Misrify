1ed58c035b4516fda6c90d9075b4f4db1e5d043e
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-22 09:54:21 +0200

    Handle wrong Routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,15 +1,13 @@
+import AppError from '../utils/AppError.js'
 import authRoutes from './auth.routes.js'
 import userRoutes from './user.routes.js'
 
 export const wrapRoutes = (app) => {
 
-    app.use((req, res, next) => {
-        res.status(404).json({
-            success: false,
-            message: "Route not found",
-        });
-    });
-
     app.use("/api/auth", authRoutes)
     app.use("/api/user", userRoutes)
+
+    app.use('*', (req, res, next) => {
+        next(new AppError('invalid routing path ' + req.originalUrl, 404));
+    });
 }
