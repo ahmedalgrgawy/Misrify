a76faf3449b5e92306b675c5fe5d8164a9fe9775
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-06 17:21:26 +0200

    User Profile Update

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,15 +1,54 @@
 import User from "../models/user.model.js"
+import AppError from "../errors/AppError.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id)
+    res.status(200).json({ success: true, user: req.user })
+}
 
-        if (!user) {
-            res.status(401).json({ success: false, message: "User Not Found" })
+export const updateProfile = async (req, res, next) => {
+    const userId = req.user.id;
+    const { name, phoneNumber, address, currentPassword, newPassword } = req.body;
+    let { imgUrl } = req.body;
+
+    let user = await User.findById(userId);
+
+    if (!user) {
+        return next(new AppError("User Not Found", 404))
+    }
+
+    if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+        return next(new AppError("Please provide both current password and new password", 400))
+    }
+
+    if (currentPassword && newPassword) {
+        const isMatch = await user.comparePassword(currentPassword);
+
+        if (!isMatch) {
+            return next(new AppError("Current password is incorrect", 400))
         }
 
-        res.status(200).json({ success: true, user })
-    } catch (error) {
-        console.log(error);
+        user.password = newPassword;
     }
+
+    if (imgUrl) {
+        if (user.imgUrl) {
+            await cloudinary.uploader.destroy(user.imgUrl.split("/").pop().split(".")[0]);
+        }
+
+        const uploadedResponse = await cloudinary.uploader.upload(imgUrl);
+        imgUrl = uploadedResponse.secure_url;
+    }
+
+    user.name = name || user.name;
+    user.phoneNumber = phoneNumber || user.phoneNumber;;
+    user.address = address || user.address;;
+    user.imgUrl = imgUrl || user.imgUrl;;
+
+    user = await user.save();
+
+    user.password = null;
+
+    return res.status(200).json({ success: true, message: "Profile updated successfully", user });
+
 }
