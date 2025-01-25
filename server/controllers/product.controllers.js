31c1ce7058e82cb4350fa2115117c7dc12d53973
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 16:10:56 +0200

    Approved Products

--- server/controllers/product.controllers.js
+++ server/controllers/product.controllers.js
@@ -11,3 +11,17 @@ export const getRequestedProducts = async (req, res) => {
 
     res.status(200).json({ success: true, RequestedProducts })
 }
+
+export const getProducts = async (req, res) => {
+    const Products = await Product.find({ isApproved: true })
+        .populate("category")
+        .populate("brand")
+        .populate("merchant")
+        .exec();
+
+    if (!Products || Products.length === 0) {
+        return res.status(404).json({ success: false, message: "No Products Found" })
+    }
+
+    res.status(200).json({ success: true, Products })
+}
