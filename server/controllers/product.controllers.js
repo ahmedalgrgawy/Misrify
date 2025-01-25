e31a945d656c5e1669307df11e5d15765d60c9c6
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 16:09:54 +0200

    Requested Products

---
+++ server/controllers/product.controllers.js
@@ -0,0 +1,13 @@
+export const getRequestedProducts = async (req, res) => {
+    const RequestedProducts = await Product.find({ isApproved: false })
+        .populate("category")
+        .populate("brand")
+        .populate("merchant")
+        .exec();
+
+    if (!RequestedProducts || RequestedProducts.length === 0) {
+        return res.status(404).json({ success: false, message: "No Requested Products Found" })
+    }
+
+    res.status(200).json({ success: true, RequestedProducts })
+}
