b72a7443cfbd758940248f408fff9ff09671d53e
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 17:57:49 +0200

    merchant:create-product

--- server/routes/merchant.routes.js
+++ server/routes/merchant.routes.js
@@ -9,5 +9,5 @@ const router = express.Router()
 
 router.get("/products", catchAsync(protectedRoute), catchAsync(merchantRoute), catchAsync(getMerchantProducts))
 
-router.post("/products", catchAsync(protectedRoute), catchAsync(merchantRoute), validate(createProductSchema), catchAsync(createProduct))
+router.post("/create-product", catchAsync(protectedRoute), catchAsync(merchantRoute), validate(createProductSchema), catchAsync(createProduct))
 export default router;
