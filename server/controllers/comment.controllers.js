222c0326a5ba797959e0af6cfc42a9a7e2c67528
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 17:23:08 +0200

    Delete Comment

---
+++ server/controllers/comment.controllers.js
@@ -0,0 +1,18 @@
+// This Function is for Admin Only
+export const deleteComment = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ success: false, message: "Comment Id is Required" })
+    }
+
+    const CommentToDelete = await Comment.findById(id);
+
+    if (!CommentToDelete) {
+        return res.status(404).json({ success: false, message: "Comment Not Found" })
+    }
+
+    await CommentToDelete.remove();
+
+    res.status(200).json({ success: true, message: "Comment Deleted" })
+}
