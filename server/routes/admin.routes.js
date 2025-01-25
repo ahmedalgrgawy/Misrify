72a79c8b61b46491e1d4b7f700f507f942ff2a86
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 16:33:08 +0200

    Create-Edit-Delete Brand

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -5,8 +5,9 @@ import { createUser, deleteUser, editUser, getAllMerchants, getAllUsers } from '
 import { validate } from '../services/validate.service.js'
 import { createUserSchema, editUserSchema } from '../validators/userValidator.js'
 import { getProducts, getRequestedProducts } from '../controllers/product.controllers.js'
-import { getBrands } from '../controllers/brand.controllers.js'
+import { createBrand, editBrand, getBrands } from '../controllers/brand.controllers.js'
 import { getAllCategories } from '../controllers/categories.controllers.js'
+import { editBrandSchema, createBrandSchema } from '../validators/brandValidator.js'
 
 const router = express.Router()
 
@@ -21,10 +22,17 @@ router.put("/edit-user/:id", catchAsync(protectedRoute), catchAsync(adminRoute),
 
 router.delete("/delete-user/:id", catchAsync(protectedRoute), catchAsync(adminRoute), validate(editUserSchema), catchAsync(deleteUser))
 
-// Handling Categories & Brands
+// Handling Categories
 router.get("/categories", catchAsync(protectedRoute), catchAsync(adminRoute), catchAsync(getAllCategories))
 
+// Handling Brands
 router.get("/brands", catchAsync(protectedRoute), catchAsync(adminRoute), catchAsync(getBrands))
+
+router.post("/create-brand", catchAsync(protectedRoute), catchAsync(adminRoute), validate(createBrandSchema), catchAsync(createBrand))
+
+router.put("/edit-brand/:id", catchAsync(protectedRoute), catchAsync(adminRoute), validate(editBrandSchema), catchAsync(editBrand))
+
+router.delete("/delete-brand/:id", catchAsync(protectedRoute), catchAsync(adminRoute), catchAsync(deleteBrand))
 
 // Handling Products
 router.get("/requested-products", catchAsync(protectedRoute), catchAsync(adminRoute), catchAsync(getRequestedProducts))
---
+++ server/validators/brandValidator.js
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const createBrandSchema = Joi.object({
+    name: Joi.string().required().messages({
+        "string.empty": "Name is required",
+    }),
+    description: Joi.string().required().messages({
+        "string.empty": "Description is required",
+    }),
+})
+
+export const editBrandSchema = Joi.object({
+    name: Joi.string().optional().messages({
+        "string.empty": "There is No Name Provided",
+    }),
+    description: Joi.string().optional().messages({
+        "string.empty": "There is No Description Provided",
+    }),
+})
