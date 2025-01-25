1629c4158070997f37aac96534b6c3c31a41f55e
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 17:35:04 +0200

    Merchant Products

---
+++ server/routes/merchant.routes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { merchantRoute, protectedRoute } from '../middlewares/auth.middlewares.js';
+import catchAsync from '../errors/catchAsync.js';
+import { createProduct, getMerchantProducts } from '../controllers/product.controllers.js';
+import { validate } from '../services/validate.service.js';
+import { createProductSchema } from '../validators/productValidator.js';
+
+const router = express.Router()
+
+router.get("/products", catchAsync(protectedRoute), catchAsync(merchantRoute), catchAsync(getMerchantProducts))
+
+router.post("/products", catchAsync(protectedRoute), catchAsync(merchantRoute), validate(createProductSchema), catchAsync(createProduct))
+export default router;
