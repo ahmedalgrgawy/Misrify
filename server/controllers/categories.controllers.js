1ad9a7be4c16a40514fc41af3e81ac08f5900579
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-25 16:41:59 +0200

    Create-Edit-Delete Category

--- server/controllers/categories.controllers.js
+++ server/controllers/categories.controllers.js
@@ -1,3 +1,5 @@
+import Category from "../models/category.model.js";
+
 export const getAllCategories = async (req, res) => {
     const Categories = await Category.find().exec();
 
@@ -6,4 +8,54 @@ export const getAllCategories = async (req, res) => {
     }
 
     res.status(200).json({ success: true, Categories })
+}
+
+export const createCategory = async (req, res) => {
+    const { name, description } = req.body;
+
+    const category = new Category({ name, description });
+
+    await category.save();
+
+    res.status(201).json({ success: true, message: "Category Created Successfully" })
+}
+
+export const editCategory = async (req, res) => {
+    const categoryId = req.params.id;
+    const { name, description } = req.body;
+
+    if (!categoryId) {
+        return res.status(400).json({ success: false, message: "Category ID is required" })
+    }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+        return res.status(404).json({ success: false, message: "Category not found" })
+    }
+
+    category.name = name || category.name;
+    category.description = description || category.description;;
+
+    await category.save();
+
+    res.status(200).json({ success: true, message: "Category updated successfully", category })
+}
+
+export const deleteCategory = async (req, res) => {
+    const categoryId = req.params.id;
+
+    if (!categoryId) {
+        return res.status(400).json({ success: false, message: "Category ID is required" })
+    }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+        return res.status(404).json({ success: false, message: "Category not found" })
+    }
+
+    await category.deleteOne();
+
+    res.status(200).json({ success: true, message: "Category deleted successfully" })
 }
