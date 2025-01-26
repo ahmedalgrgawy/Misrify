97cc5b9d5ba640b7f7768c7060a52acdd266815a
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-01-26 10:01:42 +0200

    create-comment

--- server/controllers/comment.controllers.js
+++ server/controllers/comment.controllers.js
@@ -1,4 +1,6 @@
 import AppError from "../errors/AppError.js";
+import Comment from "../models/comment.model.js";
+import Product from "../models/product.model.js";
 
 // This Function is for Admin Only
 export const deleteComment = async (req, res, next) => {
@@ -19,5 +21,42 @@ export const deleteComment = async (req, res, next) => {
     res.status(200).json({ success: true, message: "Comment Deleted" })
 }
 
-// This Function is for Merchant Only
-// TODO: Implement Create Comment for Merchant
+
+export const createComment = async (req, res, next) => {
+    const { productId, reviewId, text } = req.body;
+    const userId = req.user._id;
+
+    if (!productId) {
+        return next(new AppError("Product ID is required", 400));
+    }
+
+    if (!text) {
+        return next(new AppError("Text is required", 400));
+    }
+
+    const product = await Product.findById(productId).populate({
+        path: 'reviews.comments',
+        populate: {
+            path: 'user',
+            select: 'name email'
+        }
+    });
+
+    if (!product) {
+        return next(new AppError("Product Not Found", 404));
+    }
+
+    const newComment = await Comment.create({
+        text,
+        user: userId
+    });
+
+    product.reviews.comments.push(newComment._id);
+
+    await product.save();
+
+    res.status(201).json({
+        success: true,
+        comment: newComment
+    });
+};
