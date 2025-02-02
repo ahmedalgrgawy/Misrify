eb0de65028a9b018eea465ea953b75ca3d4cab3a
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-02-02 23:12:59 +0200

    Get - Toggle Wishlist

--- server/controllers/wishlist.controllers.js
+++ server/controllers/wishlist.controllers.js
@@ -15,5 +15,37 @@ export const getWishlist = async (req, res, next) => {
 }
 
 export const toggleWishlist = async (req, res, next) => {
+    const userId = req.user._id
+    const { productId } = req.body
 
+    // check if user has wishlist in db < if not create one and add the product >
+    const wishlist = await Wishlist.findOne({ user: userId })
+
+    if (!wishlist) {
+
+        const newWishlist = await Wishlist.create({ user: userId })
+
+        newWishlist.wishlistItems.push(productId)
+
+        await newWishlist.save()
+
+        return res.status(200).json({ success: true, message: "Product Added To Wishlist" })
+    }
+
+    console.log(wishlist)
+
+    // Check if product is already in wishlist < if there then remove else add >
+    if (wishlist.wishlistItems.includes(productId)) {
+
+        wishlist.wishlistItems = wishlist.wishlistItems.filter((item) => item.toString() !== productId.toString())
+
+        await wishlist.save()
+        console.log(wishlist)
+        return res.status(200).json({ success: true, message: "Product Removed From Wishlist" })
+    }
+
+    wishlist.wishlistItems.push(productId)
+    await wishlist.save()
+
+    res.status(200).json({ success: true, message: "Product Added To Wishlist" })
 }
