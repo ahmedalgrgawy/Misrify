09231205c31825e31fa127d7cdedc1a1ceb60feb
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-03-22 23:06:51 +0200

    fix:wishlist

--- server/controllers/wishlist.controllers.js
+++ server/controllers/wishlist.controllers.js
@@ -1,4 +1,5 @@
 import AppError from "../errors/AppError.js"
+import Product from "../models/product.model.js"
 import Wishlist from "../models/wishlist.model.js"
 
 export const getWishlist = async (req, res, next) => {
@@ -9,7 +10,9 @@ export const getWishlist = async (req, res, next) => {
 
     if (!wishlist) return next(new AppError("Wishlist Not Found", 404))
 
-    if (wishlist.wishlistItems.length === 0) return next(new AppError("Wishlist Is Empty", 404))
+    if (wishlist.wishlistItems.length === 0) {
+        return res.status(200).json({ success: true, message: "Wishlist Is Empty", wishlist: [] })
+    }
 
     res.status(200).json({ success: true, wishlist })
 }
@@ -32,7 +35,7 @@ export const toggleWishlist = async (req, res, next) => {
         return res.status(200).json({ success: true, message: "Product Added To Wishlist" })
     }
 
-    console.log(wishlist)
+    // console.log(wishlist)
 
     // Check if product is already in wishlist < if there then remove else add >
     if (wishlist.wishlistItems.includes(productId)) {
@@ -43,6 +46,11 @@ export const toggleWishlist = async (req, res, next) => {
         console.log(wishlist)
         return res.status(200).json({ success: true, message: "Product Removed From Wishlist" })
     }
+
+    const product = await Product.findById(productId)
+
+    const productExists = await Product.exists({ _id: productId });
+    if (!productExists) return next(new AppError("Product not found", 404));
 
     wishlist.wishlistItems.push(productId)
     await wishlist.save()
