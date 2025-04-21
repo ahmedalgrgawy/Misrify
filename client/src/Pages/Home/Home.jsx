7adf9e33cd74509a5dfebe11fd10a74468c47854
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2025-04-21 17:33:51 +0200

    Integrate Cal

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import style from "./Home.module.css";
 import HeroPic from "../../assets/home imgs/HeroPic.png";
 import Versace from "../../assets/home imgs/versace.png";
@@ -17,8 +18,14 @@ import star2 from "../../assets/home imgs/Sstar.png";
 import { PiDownloadSimpleBold } from "react-icons/pi";
 import { BiSolidLike } from "react-icons/bi";
 import { TiStarFullOutline } from "react-icons/ti";
+import CalScheduler from "../../Components/Cal/CalScheduler";
 
 const Home = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => setIsPopupOpen(true);
+  const closePopup = () => setIsPopupOpen(false);
+
   return (
     <>
       <div className="px-10 md:px-24 pt-16">
@@ -28,21 +35,20 @@ const Home = () => {
               Egyptian <br /> E-commerce
             </h2>
             <p className={`${style.heroDescription} mb-6`}>
-              Egypt's e-commerce sector has witnessed significant growth in
-              recent years, fueled by increasing internet penetration,a large
+              Egypt&apos;s e-commerce sector has witnessed significant growth in
+              recent years, fueled by increasing internet penetration, a large
               youthful population, and rising consumer demand for convenience
               and a wide range of products.
             </p>
             <button
               className={`${style.btnMeeting} mb-16 py-3 px-8 rounded-3xl hover:bg-main-blue transition-all text-white bg-title-blue`}
+              onClick={openPopup}
             >
               Meeting now
             </button>
 
             <div className="flex flex-col md:flex-row md:justify-between w-full">
-              <div
-                className={`${style.info} text-center mb-5 md:mb-0 md:w-1/3 `}
-              >
+              <div className={`${style.info} text-center mb-5 md:mb-0 md:w-1/3`}>
                 <h5>200+</h5>
                 <p>Local Brands</p>
               </div>
@@ -54,9 +60,7 @@ const Home = () => {
                 <p>High-Quality Products</p>
               </div>
 
-              <div
-                className={`${style.info} text-center mb-5 md:mb-0 md:w-1/3 `}
-              >
+              <div className={`${style.info} text-center mb-5 md:mb-0 md:w-1/3`}>
                 <h5>30,000+</h5>
                 <p>Happy Customers</p>
               </div>
@@ -79,8 +83,8 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="pb-20 px-20 md:px-24  bg-dark-blue">
-        <div className="flex flex-wrap justify-center gap-10 xl:justify-between p-8 border-b-2 ">
+      <div className="pb-20 px-20 md:px-24 bg-dark-blue">
+        <div className="flex flex-wrap justify-center gap-10 xl:justify-between p-8 border-b-2">
           <img src={Versace} alt="Versace Logo" />
           <img src={Zara} alt="Zara Logo" />
           <img src={Gucci} alt="Gucci Logo" />
@@ -89,10 +93,8 @@ const Home = () => {
         </div>
 
         <div>
-          <h2 className={`${style.categoriesTitle} my-24`}>
-            Find Your Categories
-          </h2>
-          <div className="grid grid-cols-12 gap-5 grid-rows-3 xl:grid-rows-2 ">
+          <h2 className={`${style.categoriesTitle} my-24`}>Find Your Categories</h2>
+          <div className="grid grid-cols-12 gap-5 grid-rows-3 xl:grid-rows-2">
             <div className="flex flex-col justify-around md:justify-start col-span-12 xl:col-span-4 2xl:col-span-4 bg-white rounded-3xl p-8">
               <p
                 className={`${style.categoryTitles} mb-5 order-2 md:order-1 text-center md:text-start`}
@@ -100,13 +102,13 @@ const Home = () => {
                 Shoes
               </p>
               <img
-                className=" md:self-end order-1 md:order-2"
+                className="md:self-end order-1 md:order-2"
                 src={Shoes}
                 alt="Shoes"
               />
             </div>
 
-            <div className="col-span-12 xl:col-span-8 2xl:col-span-8 flex flex-col md:flex-row  justify-between bg-white rounded-3xl overflow-hidden">
+            <div className="col-span-12 xl:col-span-8 2xl:col-span-8 flex flex-col md:flex-row justify-between bg-white rounded-3xl overflow-hidden">
               <p
                 className={`${style.categoryTitles} p-8 order-2 md:order-1 text-center md:text-start`}
               >
@@ -119,7 +121,7 @@ const Home = () => {
               />
             </div>
 
-            <div className="flex flex-col md:flex-row justify-between col-span-12 xl:col-span-7 2xl:col-span-8 bg-white rounded-3xl ">
+            <div className="flex flex-col md:flex-row justify-between col-span-12 xl:col-span-7 2xl:col-span-8 bg-white rounded-3xl">
               <p
                 className={`${style.categoryTitles} p-8 order-2 md:order-1 text-center md:text-start`}
               >
@@ -155,38 +157,33 @@ const Home = () => {
           className={`${style.download} text-center md:text-start w-full xl:w-1/2 mt-10 xl:mt-0`}
         >
           <h2 className="text-dark-blue mb-2">Download App Now</h2>
-
           <p className="mb-11">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
             nunc ante velit vitae. Est tellus vitae, nullam lobortis enim.
             Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.
             Ullamcorper risus tempor, ac nunc libero urna, feugiat.
           </p>
-
           <div className="flex justify-around lg:justify-start flex-wrap mb-16">
             <img className="md:me-7" src={GooglePlay} alt="GooglePlay Logo" />
             <img src={AppStore} alt="AppStore Logo" />
           </div>
-
           <div className="flex justify-around xl:justify-between md:flex-row flex-col">
             <div
-              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44  ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
+              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44 ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
             >
               <PiDownloadSimpleBold className="text-4xl" />
               <p>59865</p>
               <p>Download</p>
             </div>
-
             <div
-              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44  ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
+              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44 ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
             >
               <BiSolidLike className="text-4xl" />
               <p>29852</p>
               <p>LIke</p>
             </div>
-
             <div
-              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44  ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
+              className={`flex flex-col bg-dark-blue text-white md:mb-0 mb-5 md:w-28 md:px-3 xl:w-44 ${style.ratings} rounded-xl py-5 2xl:px-1 box-content text-center items-center justify-between`}
             >
               <TiStarFullOutline className="text-4xl" />
               <p>1500</p>
@@ -194,11 +191,12 @@ const Home = () => {
             </div>
           </div>
         </div>
-
         <div className="w-full xl:w-1/2 mt-10 xl:mt-0 flex justify-center xl:justify-end">
           <img className="" src={AppPic} alt="Appliction's picture" />
         </div>
       </div>
+
+      <CalScheduler isOpen={isPopupOpen} onClose={closePopup} />
     </>
   );
 };
