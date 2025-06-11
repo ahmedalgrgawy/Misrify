49a32b3e9d50fdc1b69236ce9cd8b537dbee9cea
sarah elkholy <you@example.com>
2025-06-11 19:52:14 +0300

    edits

--- mobile/lib/views/profile/widgets/help and support/widget_help.dart
+++ mobile/lib/views/profile/widgets/help and support/widget_help.dart
@@ -5,13 +5,12 @@ import 'package:graduation_project1/constants/constants.dart';
 import 'package:graduation_project1/views/profile/help_and_support_screen.dart';
 
 class FAQPage extends StatelessWidget {
-  static const String routeName = "widget_help";
   const FAQPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Kbackground, // light background
+      backgroundColor: Kbackground,
       appBar: AppBar(
         title: ReusableText(
           text: 'Asked Questions',
