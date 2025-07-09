4fa49335f6bb397ba10aaba1a4cb1b9e8303b772
sarah elkholy <you@example.com>
2025-07-09 19:01:33 +0300

    Upload full project to MISRIFY repo

--- mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter_windows_3.7.12-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Graduation-Project\mobile"
+export "FLUTTER_APPLICATION_PATH=E:\flutter_apps\Graduation-Project\mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
